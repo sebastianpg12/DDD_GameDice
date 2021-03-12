f5eca09a4bf52e239d2716ea6bc5c0475f72bfcb
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-03-12 16:51:49 -0500

    Creación de un proyecto base con spring boot initializr

---
+++ src/main/java/co/com/sofka/DDD_GameDice/DddGameDiceApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.DDD_GameDice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddGameDiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddGameDiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/DDD_GameDice/DddGameDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.DDD_GameDice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddGameDiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
